a5c692fad113e0e731b730d81a9dd4be30b144e6
PARK SINYOUNG <92427132+parknew0@users.noreply.github.com>
2025-02-04 23:58:17 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 748 ms, Memory: 59808 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -2,15 +2,6 @@
 import java.io.*;
 import java.util.*;
 
-class CoorComparator implements Comparator<int []> {
-    @Override
-    public int compare(int[] a1, int[] a2)
-    {
-        if (a1[1] != a2[1]) return Integer.compare(a1[1], a2[1]);
-        else return Integer.compare(a1[0], a2[0]);
-    }
-}
-
 public class Main {
     public static void main(String[] args) throws IOException{
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,7 +17,7 @@ public class Main {
         }
       }
 
-      Arrays.sort(coordinates, new CoorComparator());
+      Arrays.sort(coordinates, (a1, a2) -> a1[1] != a2[1] ? Integer.compare(a1[1],a2[1]) : Integer.compare(a1[0],a2[0]));
 
       for (int[] row : coordinates) {
           bw.write(row[0] + " " + row[1] + "\n");
