43f6a1f878d2d3290c185ce23bb7a265055b5d4f
PARK SINYOUNG <92427132+parknew0@users.noreply.github.com>
2025-02-24 01:51:35 +0900

    [Silver I] Title: IOIOI, Time: 236 ms, Memory: 20100 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,6 +1,5 @@
 
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
@@ -9,19 +8,45 @@ public class Main {
     int m = Integer.parseInt(br.readLine());
     String s = br.readLine();
 
-    StringBuilder pn = new StringBuilder();
-    for (int i = 0; i < n; i++) {
-        pn.append("IO");
-    }
-    pn.append("I");
-    int pnLen = pn.length();
-
     int count = 0;
-    for (int i = 0; i < m - pnLen + 1; i++) {
-        String sub = s.substring(i, i + pnLen);
-        if (sub.equals(pn.toString())) count++;
+    int sum = 0;
+    int prevChar = s.charAt(0);
+    if (prevChar == 'I') {
+        count = 1;
     }
 
-    System.out.println(count);
+    for (int i = 1; i < m; i++) {
+        char c = s.charAt(i);
+        if (c != prevChar && c == 'I') {
+            count++;
+            if (i == m - 1) {
+                int oCount = (count > 1) ? count - 1 : 0;
+                int temp = (oCount - n + 1) >= 0 ? oCount - n + 1 : 0;
+                sum += temp;
+            }
+        }
+        if (c != prevChar && c == 'O') {
+            if (i == m - 1) {
+                int oCount = (count > 1) ? count - 1 : 0;
+                int temp = (oCount - n + 1) >= 0 ? oCount - n + 1 : 0;
+                sum += temp;
+            }
+        }
+        if (c == prevChar && c == 'I') {
+            int oCount = (count > 1) ? count - 1 : 0;
+            int temp = (oCount - n + 1) >= 0 ? oCount - n + 1 : 0;
+            sum += temp;
+            count = 1;
+        }
+        if (c == prevChar && c == 'O') {
+            int oCount = (count > 1) ? count - 1 : 0;
+            int temp = (oCount - n + 1) >= 0 ? oCount - n + 1 : 0;
+            sum += temp;
+            count = 0;
+        }
+        prevChar = c;
+    }
+
+    System.out.println(sum);
     }
 }
