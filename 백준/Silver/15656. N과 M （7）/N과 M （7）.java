ebc6b457077833a60a3f5b5177ad87b4bd522c08
PARK SINYOUNG <92427132+parknew0@users.noreply.github.com>
2025-03-26 15:52:15 +0900

    [Silver III] Title: N과 M (7), Time: 552 ms, Memory: 122344 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,44 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] arr;
+    public static int[] result;
+    static int n, m;
+
+    public static void main(String[] args) throws IOException {
+        String[] tokens = br.readLine().split(" ");
+        n = Integer.parseInt(tokens[0]);
+        m = Integer.parseInt(tokens[1]);
+        arr = new int[n];
+        tokens = br.readLine().split(" ");
+        for (int i = 0 ; i < n ; i++) {
+            arr[i] = Integer.parseInt(tokens[i]);
+        }
+        Arrays.sort(arr);
+        result = new int[m];
+
+        solve(0);
+
+        System.out.print(sb);
+    }
+
+    public static void solve (int depth) {
+        if (depth == m) {
+            for (int i = 0 ; i < m ; i++) {
+                sb.append(arr[result[i]]);
+                if (i < m - 1) sb.append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0 ; i < n ; i++) {
+            result[depth] = i;
+            solve(depth + 1);
+        }
+    }
+}
