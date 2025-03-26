a00fc430dd8568c49e26b9c3b2c09960bdf7dbb0
PARK SINYOUNG <92427132+parknew0@users.noreply.github.com>
2025-03-26 10:07:05 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -3,34 +3,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int n;
-    public static int m;
-    public static int[] progressions;
-    public static void main(String[] args) throws IOException
-    {
-        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        progressions = new int[m];
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] result;
+    static int n, m;
 
-        Comb(1,0);
+    public static void main(String[] args) throws IOException {
+        String[] tokens = br.readLine().split(" ");
+        n = Integer.parseInt(tokens[0]);
+        m = Integer.parseInt(tokens[1]);
+        result = new int[m];
+
+        solve(1, 0);
+
+        System.out.print(sb);
     }
 
-    public static void Comb(int start, int depth)
-    {
+    public static void solve (int start, int depth) {
         if (depth == m) {
-            for (int i = 0; i < progressions.length; i++) {
-                System.out.print(progressions[i] + " ");
+            for (int i = 0 ; i < m ; i++) {
+                sb.append(result[i]);
+                if (i < m - 1) sb.append(" ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
 
-        for (int i = start; i <= n; i++) {
-            progressions[depth] = i;
-            Comb(i + 1, depth + 1);
+        for (int i = start ; i <= n ; i++) {
+            result[depth] = i;
+            solve(i + 1, depth + 1);
         }
-    }
 
+    }
 }
