c48c3a1dfa69c4daccf4067c3ee87435f0881f05
PARK SINYOUNG <92427132+parknew0@users.noreply.github.com>
2025-03-30 15:42:02 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.util.*;
 
@@ -6,39 +5,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
         int T = Integer.parseInt(br.readLine());
+        
         for (int t = 0; t < T; t++) {
             String[] tokens = br.readLine().split(" ");
             long x = Long.parseLong(tokens[0]);
             long y = Long.parseLong(tokens[1]);
-            long goalDistance = y - x;
+            long d = y - x;
             
-            // 만약 이동해야 할 거리가 0이면 0을 출력
-            if (goalDistance == 0) {
+            if (d == 0) {
                 sb.append("0").append("\n");
                 continue;
             }
             
-            long movedDistance = 0; // 누적 이동거리
-            int i = 0;             // 현재 이동 단계(한 사이클에서의 최고 이동 거리)
-            int count = 0;         // 총 이동 횟수 (사이클마다 2씩 증가)
-            
-            while (true) {
-                i++;                     // 이동 단계 증가
-                movedDistance += i * 2;  // 가속과 감속으로 i씩 두 번 이동하므로 2*i를 더함
-                count += 2;              // 한 사이클에 2번의 이동
-                
-                // 마지막 단계 없이도 목표 거리를 달성할 수 있다면, 이동 횟수를 하나 줄임
-                if (movedDistance - i >= goalDistance) {
-                    sb.append(count - 1).append("\n");
-                    break;
-                } 
-                // 누적 이동거리가 목표 거리를 넘었다면, 현재 count가 정답
-                else if (movedDistance >= goalDistance) {
-                    sb.append(count).append("\n");
-                    break;
-                }
+            long n = (long)Math.sqrt(d);
+            if(n * n == d) {
+                sb.append(2 * n - 1).append("\n");
+            }
+            else if(d <= n * n + n) {
+                sb.append(2 * n).append("\n");
+            }
+            else {
+                sb.append(2 * n + 1).append("\n");
             }
         }
         System.out.print(sb);
