ae84da9167e33d94dc784097f5d3f15a6ec7afd9
PARK SINYOUNG <92427132+parknew0@users.noreply.github.com>
2025-04-03 11:12:35 +0900

    [Gold V] Title: A와 B 2, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = br.readLine();
+        String b = br.readLine();
+
+        Queue<String> queue = new ArrayDeque<>();
+        queue.offer(b);
+        boolean isFound = false;
+        
+        while (!queue.isEmpty()) {
+            String cur = queue.poll();
+            // System.out.println(cur);
+
+            if (cur.equals(a)) {
+                isFound = true;
+                break;
+            }
+            if (cur.length() <= a.length()) continue;
+
+            if (cur.charAt(0) == 'B'){
+                String temp = cur.substring(1);
+                temp = new StringBuilder(temp).reverse().toString();
+
+                queue.offer(temp);
+            }
+
+            if (cur.charAt(cur.length() - 1) == 'A') {
+                String temp = cur.substring(0, cur.length() - 1);
+                queue.offer(temp);
+            }
+
+        }
+
+        if (isFound) System.out.print("1");
+        else System.out.print("0");
+    }
+
+}
