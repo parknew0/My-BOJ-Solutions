c6b779defbe38f986aa0dcb3ba427404914aec8b
PARK SINYOUNG <92427132+parknew0@users.noreply.github.com>
2025-05-19 22:41:50 +0900

    [Silver I] Title: 포도주 시식, Time: 128 ms, Memory: 14932 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] wine = new int[n + 1];
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            wine[i] = Integer.parseInt(br.readLine());
+        }
+        dp[1] = wine[1];
+        if (n == 1){
+            System.out.println(dp[n]);
+            return;
+        }
+        dp[2] = wine[1] + wine[2];
+
+        if (n == 2){
+            System.out.println(dp[n]);
+            return;
+        }
+
+        for (int i = 3 ; i <= n ; i++) {
+            dp[i]=Math.max(Math.max(dp[i-1],dp[i-2]+wine[i]),dp[i-3]+wine[i-1]+wine[i]);
+        }
+
+        System.out.println(dp[n]);
+    }
+}
