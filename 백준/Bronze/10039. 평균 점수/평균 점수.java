86721c3e4e530bfa3bba25e149d084257170e729
PARK SINYOUNG <92427132+parknew0@users.noreply.github.com>
2025-10-29 09:28:00 +0900

    [Bronze IV] Title: 평균 점수, Time: 172 ms, Memory: 17504 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int total = 0;
+        
+        for (int i = 0; i < 5; i++) {
+            int score = sc.nextInt();
+            if (score < 40) {
+                total += 40;
+            } else {
+                total += score;
+            }
+        }
+        
+        System.out.println(total / 5);
+        
+        sc.close();
+    }
+}
